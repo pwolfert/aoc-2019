5ccc5c58f166f3550ec7eb64eeee69a62c101da5
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2019-12-03 14:17:30 -0800

    Finished the 03 exercise with a correct answer

--- 03/wires.exs
+++ 03/wires.exs
@@ -16,13 +16,14 @@ defmodule WireGrid do
     %WireSegment{x0: x, y0: y, x1: x + dx, y1: y + dy}
   end
 
+  # In order to simplify intersection calculation, I'm normalizing the segments
+  # so that x0 is always lower than x1 and y0 is always lower than y1
   def normalize_segment(segment) do
-    %WireSegment{
-      x0: if segment.x0 < segment.x1 do segment.x0 end,
-      x1: if segment.x0 >= segment.x1 do segment.x1 end,
-      y0: if segment.y0 < segment.y1 do segment.y0 end,
-      y1: if segment.y0 >= segment.y1 do segment.y1 end
-    }
+    x0 = if segment.x0 < segment.x1, do: segment.x0, else: segment.x1
+    x1 = if segment.x0 < segment.x1, do: segment.x1, else: segment.x0
+    y0 = if segment.y0 < segment.y1, do: segment.y0, else: segment.y1
+    y1 = if segment.y0 < segment.y1, do: segment.y1, else: segment.y0
+    %WireSegment{x0: x0, x1: x1, y0: y0, y1: y1}
   end
 
   def wire_from_path(path), do: wire_from_path(path, 0, 0)
@@ -32,7 +33,8 @@ defmodule WireGrid do
     [segment | wire_from_path(tail, segment.x1, segment.y1)]
   end
 
-  def segments_intersect?(s1, s2) do
+  # Only works for normalized segments
+  def normalized_segments_intersect?(s1, s2) do
     x_overlap = (
       (s1.x0 <= s2.x0 and s1.x1 >= s2.x1) or
       (s2.x0 <= s1.x0 and s2.x1 >= s1.x1)
@@ -44,13 +46,41 @@ defmodule WireGrid do
     x_overlap and y_overlap
   end
 
+  def get_horizontal_vertical_intersection(h_segment, v_segment) do
+    relative_x = v_segment.x0 - h_segment.x0
+    relative_y = h_segment.y0 - v_segment.y0
+    {h_segment.x0 + relative_x, v_segment.y0 + relative_y}
+  end
 
+  def get_normalized_segment_intersection(s1, s2) do
+    if s1.x1 - s1.x0 > 0 do
+      # s1 is the horizontal line
+      get_horizontal_vertical_intersection(s1, s2)
+    else
+      # s2 is the horizontal line
+      get_horizontal_vertical_intersection(s2, s1)
+    end
+  end
 
   def get_wire_intersections(wire1, wire2) do
-    for seg1 <- wire1,
-        seg2 <- wire2,
-        segments_intersect?(seg1, seg2),
-        do: {seg1, seg2}
+    normalized_wire1 = Enum.map(wire1, &normalize_segment/1)
+    normalized_wire2 = Enum.map(wire2, &normalize_segment/1)
+    for seg1 <- normalized_wire1,
+        seg2 <- normalized_wire2,
+        normalized_segments_intersect?(seg1, seg2),
+        do: get_normalized_segment_intersection(seg1, seg2)
+  end
+
+  def get_distance_from_origin({x, y}) do
+    abs(x) + abs(y)
+  end
+
+  def get_closest_intersection(wire1, wire2) do
+    WireGrid.get_wire_intersections(wire1, wire2)
+      |> Enum.map(&get_distance_from_origin/1)
+      |> Enum.filter(fn distance -> distance > 0 end)
+      |> Enum.sort()
+      |> List.first()
   end
 
   def print_wire(wire_segments) do
@@ -82,8 +112,9 @@ defmodule Script do
           |> Enum.map(&WireGrid.wire_from_path/1)
         # WireGrid.print_wire(wire1)
         # WireGrid.print_wire(wire2)
-        intersections = WireGrid.get_wire_intersections(wire1, wire2)
-        IO.inspect(intersections)
+        # intersections = WireGrid.get_wire_intersections(wire1, wire2)
+        # IO.inspect(intersections)
+        IO.puts(WireGrid.get_closest_intersection(wire1, wire2))
     end
   end
 end
