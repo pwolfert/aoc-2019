c42921b7f2f4259c65f202c2d5d982dc13b098f0
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2019-12-09 10:57:41 -0800

    Added support for more opcodes

--- 05/intcode.exs
+++ 05/intcode.exs
@@ -2,27 +2,41 @@
 defmodule Intcode do
   def add(a, b), do: a + b
   def mul(a, b), do: a * b
+  def less_than(a, b), do: if a < b, do: 1, else: 0
+  def equal_to(a, b), do: if a == b, do: 1, else: 0
 
-  def get_parameter_modes(instruction) do
-    instruction
-      |> Integer.digits()
+  def get_parameter_modes(instruction, num_params \\ 2) do
+    digits = Integer.digits(instruction)
+    parameter_starting_index = min(Enum.count(digits), 2)
+    digits
       |> Enum.reverse()
-      |> Enum.slice(2, 2)
+      # If the insruction is missing parameter modes, pad with leading zeroes
+      |> Enum.concat(List.duplicate(0, num_params))
+      |> Enum.slice(parameter_starting_index, num_params)
+  end
+
+  def get_parameter_values(ints, instruction, parameters) do
+    parameters
+      |> Enum.zip(get_parameter_modes(instruction, Enum.count(parameters)))
+      |> Enum.map(fn {param, mode} ->
+        if mode == 1 do
+          # As value
+          param
+        else
+          # As pointer
+          Enum.fetch!(ints, param)
+        end
+      end)
   end
 
   def process_binary_op(ints, pos, op) do
-    instruction = Enum.fetch!(ints, pos)
-    [a, b, out_pos] = Enum.slice(ints, pos + 1, 3)
-    parameter_modes = get_parameter_modes(instruction)
-    a_mode = Enum.at(parameter_modes, 0, 0)
-    b_mode = Enum.at(parameter_modes, 1, 0)
-    a = if a_mode !== 1, do: Enum.fetch!(ints, a), else: a
-    b = if b_mode !== 1, do: Enum.fetch!(ints, b), else: b
+    [instruction, a, b, out_pos] = Enum.slice(ints, pos, 4)
+    [a, b] = get_parameter_values(ints, instruction, [a, b])
     out = op.(a, b)
     process(List.replace_at(ints, out_pos, out), pos + 4)
   end
 
-  def process_input_op(ints, pos) do
+  def process_input(ints, pos) do
     out_pos = Enum.fetch!(ints, pos + 1)
     input = IO.gets("Enter an integer: ")
       |> String.trim()
@@ -30,10 +44,21 @@ defmodule Intcode do
     process(List.replace_at(ints, out_pos, input), pos + 2)
   end
 
-  def process_output_op(ints, pos) do
+  def process_output(ints, pos) do
     out_pos = Enum.fetch!(ints, pos + 1)
     IO.puts("#{Enum.fetch!(ints, out_pos)}")
     process(ints, pos + 2)
+  end
+
+  def process_jump_if(ints, pos, truthy) do
+    [instruction, value, jump_to] = Enum.slice(ints, pos, 3)
+    [value, jump_to] = get_parameter_values(ints, instruction, [value, jump_to])
+    jump? = (truthy and value != 0) or (!truthy and value === 0)
+    if jump? do
+      process(ints, jump_to)
+    else
+      process(ints, pos + 3)
+    end
   end
 
   def process(ints, pos) do
@@ -42,8 +67,12 @@ defmodule Intcode do
     case opcode do
       1 -> process_binary_op(ints, pos, &add/2)
       2 -> process_binary_op(ints, pos, &mul/2)
-      3 -> process_input_op(ints, pos)
-      4 -> process_output_op(ints, pos)
+      3 -> process_input(ints, pos)
+      4 -> process_output(ints, pos)
+      5 -> process_jump_if(ints, pos, true)
+      6 -> process_jump_if(ints, pos, false)
+      7 -> process_binary_op(ints, pos, &less_than/2)
+      8 -> process_binary_op(ints, pos, &equal_to/2)
       99 -> ints
     end
   end
@@ -73,7 +102,7 @@ defmodule Script do
   end
 
   def main(args) do
-    if Enum.count(args) === 1 do
+    if Enum.count(args) == 1 do
       input = read_input(args)
       Intcode.run(input)
     else
