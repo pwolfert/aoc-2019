362e4131c296e7d5e6afb723a6d724fbae8b1992
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2019-12-18 16:47:42 -0800

    Working solution for part 2 of day 07

--- 07/part2.exs
+++ 07/part2.exs
@@ -4,12 +4,12 @@ defmodule Intcode do
     defstruct [:ints, :inputs, :outputs, :pos]
   end
 
-  def add(a, b), do: a + b
-  def mul(a, b), do: a * b
-  def less_than(a, b), do: if a < b, do: 1, else: 0
-  def equal_to(a, b), do: if a == b, do: 1, else: 0
+  defp add(a, b), do: a + b
+  defp mul(a, b), do: a * b
+  defp less_than(a, b), do: if a < b, do: 1, else: 0
+  defp equal_to(a, b), do: if a == b, do: 1, else: 0
 
-  def get_parameter_modes(instruction, num_params \\ 2) do
+  defp get_parameter_modes(instruction, num_params) do
     digits = Integer.digits(instruction)
     parameter_starting_index = min(Enum.count(digits), 2)
     digits
@@ -19,7 +19,7 @@ defmodule Intcode do
       |> Enum.slice(parameter_starting_index, num_params)
   end
 
-  def get_parameter_values(ints, instruction, parameters) do
+  defp get_parameter_values(ints, instruction, parameters) do
     parameters
       |> Enum.zip(get_parameter_modes(instruction, Enum.count(parameters)))
       |> Enum.map(fn {param, mode} ->
@@ -33,7 +33,7 @@ defmodule Intcode do
       end)
   end
 
-  def process_binary_op(state, op) do
+  defp process_binary_op(state, op) do
     [instruction, a, b, out_pos] = Enum.slice(state.ints, state.pos, 4)
     [a, b] = get_parameter_values(state.ints, instruction, [a, b])
     out = op.(a, b)
@@ -45,29 +45,29 @@ defmodule Intcode do
     })
   end
 
-  def process_input(state) do
+  defp process_input(state, halt_on_input) do
     out_pos = Enum.fetch!(state.ints, state.pos + 1)
     [input | inputs] = state.inputs
-    process(%{
+    new_state = %{
       state
       | ints: List.replace_at(state.ints, out_pos, input),
         inputs: inputs,
         pos: state.pos + 2
-    })
-  end
-
-  def process_output(state, halt_on_output) do
-    [instruction, out] = Enum.slice(state.ints, state.pos, 2)
-    [out] = get_parameter_values(state.ints, instruction, [out])
-    new_state = %{state | outputs: state.outputs ++ [out], pos: state.pos + 2}
-    if halt_on_output do
+    }
+    if halt_on_input do
       new_state
     else
       process(new_state)
     end
   end
 
-  def process_jump_if(state, truthy) do
+  defp process_output(state) do
+    [instruction, out] = Enum.slice(state.ints, state.pos, 2)
+    [out] = get_parameter_values(state.ints, instruction, [out])
+    %{state | outputs: state.outputs ++ [out], pos: state.pos + 2}
+  end
+
+  defp process_jump_if(state, truthy) do
     [instruction, value, jump_to] = Enum.slice(state.ints, state.pos, 3)
     [value, jump_to] = get_parameter_values(state.ints, instruction, [value, jump_to])
     jump? = (truthy and value != 0) or (!truthy and value == 0)
@@ -78,28 +78,26 @@ defmodule Intcode do
     end
   end
 
-  def process(state, halt_on_output \\ false) do
+  defp process(state, halt_on_input \\ false) do
     instruction = Enum.fetch!(state.ints, state.pos)
     opcode = rem(instruction, 100)
     case opcode do
       1 -> process_binary_op(state, &add/2)
       2 -> process_binary_op(state, &mul/2)
-      3 -> process_input(state)
-      4 -> process_output(state, halt_on_output)
+      3 -> process_input(state, halt_on_input)
+      4 -> process_output(state)
       5 -> process_jump_if(state, true)
       6 -> process_jump_if(state, false)
       7 -> process_binary_op(state, &less_than/2)
       8 -> process_binary_op(state, &equal_to/2)
-      99 -> {:done, state}
+      99 -> :done
     end
   end
 
-  def run(ints, inputs, halt_on_output \\ false) do
-    process(get_initial_state(ints, inputs), halt_on_output)
+  def run(state, inputs \\ nil, halt_on_input \\ false) do
+    state = if inputs, do: %{state | inputs: inputs}, else: state
+    process(state, halt_on_input)
   end
-
-  def halt_on_output(state), do: run(state, true)
-  def halt_on_output(state, inputs), do: run(%{state | inputs: inputs}, true)
 
   def get_initial_state(ints, inputs \\ []) do
     %State{ints: ints, inputs: inputs, outputs: [], pos: 0}
@@ -121,44 +119,39 @@ defmodule AmplifierController do
     for elem <- list, rest <- permute(list -- [elem]), do: [elem | rest]
   end
 
-  def get_amplifier_array_output(amplifier_states, input) do
-    # Keep state and recurse until we get a {:done, state} response
+  def get_amplifier_loop_output(amplifier_states, input) do
     {last_output, states} = Enum.reduce(
       amplifier_states,
       {input, []},
       fn state, {previous_output, states} ->
-        case Intcode.halt_on_output(state, [previous_output]) do
-          {:done, new_state} -> {List.last(new_state.outputs), states ++ [:done]}
-          new_state -> {List.last(new_state.outputs), states ++ [new_state]}
+        new_state = Intcode.run(state, [previous_output])
+        case new_state do
+          :done -> {previous_output, states ++ [:done]}
+          _ ->
+            output = List.last(new_state.outputs)
+            {output, states ++ [new_state]}
         end
       end
     )
-    if Enum.all(states, fn state -> state == :done end) do
+
+    if Enum.any?(states, fn state -> state == :done end) do
       last_output
     else
-      get_amplifier_array_output(states, last_output)
+      get_amplifier_loop_output(states, last_output)
     end
-  end
-
-  def get_amplifier_loop_output(intcodes, amplifier_settings) do
-    {last_output, states} = Enum.reduce(
-      amplifier_settings,
-      {0, []},
-      fn phase_setting, {previous_output, states} ->
-        initial_state = Intcode.get_initial_state(intcodes, [phase_setting, previous_output])
-        case Intcode.halt_on_output(initial_state) do
-          {:done, state} -> {List.last(state.outputs), states ++ [:done]}
-          state -> {List.last(state.outputs), states ++ [state]}
-        end
-      end
-    )
-    get_amplifier_array_output(states, last_output)
   end
 
   def find_max_amplification_config(intcodes) do
     @phase_settings
       |> permute()
-      |> Enum.map(fn settings -> get_amplifier_array_output(intcodes, settings) end)
+      |> Enum.map(fn amplifier_settings ->
+        amplifier_settings
+          |> Enum.map(fn phase_setting ->
+            initial_state = Intcode.get_initial_state(intcodes)
+            Intcode.run(initial_state, [phase_setting], true)
+          end)
+          |> get_amplifier_loop_output(0)
+      end)
       |> Enum.max()
   end
 end
